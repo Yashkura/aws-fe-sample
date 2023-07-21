5b22c291e092c777a19b448f0f3768486da0952a
Yashkura <yaswanth.yash008@gmail.com>
2023-07-21 23:12:28 +0530

    build

---
+++ precache-manifest.2303a92d4d234eaf61403264a31a2c49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27538c2843cef0a2ac9d2f574ee6624c",
+    "url": "/aws-fe-sample/index.html"
+  },
+  {
+    "revision": "f9f6eef2b16874e1e3a5",
+    "url": "/aws-fe-sample/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "350b53338948d5b033d5",
+    "url": "/aws-fe-sample/static/js/2.64c5b7d3.chunk.js"
+  },
+  {
+    "revision": "4b05817eac4fd2591d59a0fba6cb5332",
+    "url": "/aws-fe-sample/static/js/2.64c5b7d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9f6eef2b16874e1e3a5",
+    "url": "/aws-fe-sample/static/js/main.5718cd99.chunk.js"
+  },
+  {
+    "revision": "506e4e0232630b5f4a6b",
+    "url": "/aws-fe-sample/static/js/runtime-main.a5faddc1.js"
+  },
+  {
+    "revision": "4f86da320ef9e6dd9442e1f33f6d6964",
+    "url": "/aws-fe-sample/static/media/image.4f86da32.jpg"
+  }
+]);
